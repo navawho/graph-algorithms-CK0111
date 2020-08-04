ebe301786a4c3775a9d1dc5d941acb397d0892ca
Matheus Navarro <matheusnavarro@alu.ufc.br>
2020-08-04 04:24:42 -0300

    funcionalidade estatica botoes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Graph } from 'react-d3-graph';
 
-import { Container, Content } from './styles';
+import { Container, Content, GraphDiv, Buttons } from './styles';
+
+const graphConfig = {
+	node: {
+		color: 'lightgreen',
+		size: 700,
+		fontSize: 16,
+	},
+	link: {
+		strokeWidth: 5,
+	},
+};
 
 const Home: React.FC = () => {
+	const [nodes, setNodes] = useState<{ id: string }[]>([{ id: '1' }]);
+	const [links, setLinks] = useState<{ source: string; target: string }[]>([]);
+
+	const handleAddNode = (): void => {
+		setNodes([...nodes, { id: '4' }]);
+	};
+
+	const handleAddLink = (): void => {
+		setLinks([...links, { source: '1', target: '4' }]);
+	};
+
 	return (
 		<Container>
 			<Content>
-
+				<Buttons>
+					<button type="button" onClick={handleAddLink}>
+						Adicionar vértice
+					</button>
+					<button type="button" onClick={handleAddNode}>
+						Adicionar aresta
+					</button>
+				</Buttons>
+				<GraphDiv>
+					<Graph id="graph-id" data={{ nodes, links }} config={graphConfig} />
+				</GraphDiv>
 			</Content>
 		</Container>
 	);
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Container = styled.div`
 	height: 100vh;
@@ -12,4 +13,34 @@ export const Content = styled.div`
 	justify-content: center;
 	align-items: center;
 	width: 100%;
+`;
+
+export const GraphDiv = styled.div`
+	background: white;
+	border-radius: 30px;
+	box-shadow: 0px 0px 20px rgba(1, 1, 1, 1);
+	padding: 12px;
+`;
+
+export const Buttons = styled.div`
+	display: flex;
+	margin-bottom: 32px;
+
+	button {
+		background: white;
+		border: 0;
+		padding: 12px;
+		border-radius: 20px;
+		margin-left: 12px;
+
+		&:first-child {
+			margin-left: 0;
+		}
+
+		transition: 0.3s;
+
+		&:hover {
+			background: ${shade(0.2, '#FFF')};
+		}
+	}
 `;
