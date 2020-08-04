1cb02c7e3f0c39a79d91266121ff06d2bf10297f
Matheus Navarro <matheusnavarro@alu.ufc.br>
2020-08-04 03:38:02 -0300

    setup

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	height: 100vh;
+	display: flex;
+	align-items: stretch;
+`;
+
+export const Content = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+`;
---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		outline: 0;
+	}
+
+	body {
+		background: #212422;
+		color: #fff;
+		-webkit-font-smoothing: antialiased;
+	}
+
+	body, input, button {
+		font-family: 'Roboto Slab', serif;
+		font-size: 16px;
+	}
+
+	button {
+		cursor: pointer;
+	}
+`;
